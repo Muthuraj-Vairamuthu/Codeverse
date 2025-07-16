0a73e494164e2a5f8d0c82aa814e85c8c85089c8
Muthuraj-Vairamuthu <muthuraj22307@iiitd.ac.in>
2025-07-17 01:22:15 +0530

    Update Settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-in-build-phase")
 
-if not SECRET_KEY:
-    raise Exception("SECRET_KEY is missing in environment!")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
